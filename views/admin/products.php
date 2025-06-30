9867e01130d47d525a921dc1006c096d1cd70cfe
JoelTobrix <pepedariojoel007@gmail.com>
2025-06-30 10:30:17 -0500

    opciones

--- views/admin/products.php
+++ views/admin/products.php
@@ -14,7 +14,15 @@
 <body>
     <div class="d-flex justify-content-between align-items-center px-4 py-2" style="background-color: #62aaf1;">
     <h5 class="mb-0">Inventario de Productos</h5>
-    <a class="btn btn-outline-primary"><img src="/interpc/icons/alarm_alert.svg" alt="Uptade" width="20" height="20">Notificaciones</a>
+    <a class="btn btn-outline-primary" onclick="mostrarNotificacion()"><img src="/interpc/icons/alarm_alert.svg" alt="Uptade" width="20" height="20">Notificaciones</a>
+          <!--Mostrar notificacion prueba-->
+       <script>
+     function mostrarNotificacion() {
+    const toast = new bootstrap.Toast(document.getElementById('miToast'));
+    toast.show();
+  }
+    
+    </script>
     <a href="/interpc/index.html" class="btn btn-outline-primary"><img src="/interpc/icons/exit.svg" alt="Uptade" width="20" height="20">Salir</a>
   </div>
 
@@ -205,6 +213,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
 <!-- Bootstrap JS y dependencias -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+<!--Mostrar notificacion-->
+<div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+  <div id="miToast" class="toast align-items-center text-bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="d-flex">
+      <div class="toast-body">
+        🔔 ¡Tienes una nueva notificación!
+      </div>
+      <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Cerrar"></button>
+    </div>
+  </div>
+</div>
 
 
 </body>
